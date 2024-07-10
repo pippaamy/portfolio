8b2e5b2729cf010b22391a2790dd8a25f1ab1da0
pippaamy <pippa_austin@live.com>
2024-07-10 15:19:03 +0100

    added dynamic nav bar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -2,30 +2,31 @@ import Header from "./Header";
 
 export default function NavBar() {
   return (
-    <div className="flex  fixed  bg-indigo-400  ">
+    <div className="p-3 flex bg-indigo-400 justify-between ">
       <Header />
       <a
         href="/"
-        className="block rounded-md px-32 py-2 text-base font-medium  hover:bg-violet-500 hover:text-white font-mono tracking-widest"
+        className="
+       rounded-md p-3 text-base font-medium  hover:text-white font-mono tracking-widest "
       >
         Home
       </a>
       <a
         href="/skills"
-        className="block rounded-md px-32 py-2 text-base font-medium  hover:bg-violet-500 hover:text-white font-mono tracking-widest"
+        className="p-3 rounded-md  text-base font-medium   hover:text-white font-mono tracking-widest"
         aria-current="page"
       >
         About
       </a>
       <a
         href="/projects"
-        className="block rounded-md px-32 py-2 text-base font-medium  hover:bg-violet-500 hover:text-white font-mono tracking-widest"
+        className="p-3 rounded-md  text-base font-medium   hover:text-white font-mono tracking-widest"
       >
         Projects
       </a>
       <a
         href="/contact"
-        className="block rounded-md px-32 py-2 text-base font-medium  hover:bg-violet-500 hover:text-white font-mono tracking-widest"
+        className=" p-3 rounded-md  text-base font-medium   hover:text-white font-mono tracking-widest"
       >
         Contact me
       </a>
